7f7b4508aa0f807792ea8459726e53f732360bbb
Andy Ryan <andyryan84874@gmail.com>
2023-07-21 19:58:28 +0000

    First commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import {VueTelInput} from 'vue-intl-phone-input';
+import 'vue-intl-phone-input/vue-tel-input.css';
 
-createApp(App).mount('#app')
+const globalOptions = {
+  mode: 'auto',
+};
+
+const app = createApp(App);
+app.use(VueTelInput, globalOptions); // Define default global options here (optional)
+app.mount('#app');
